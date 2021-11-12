deec91f054becc80c97ee8829f1f05f0c2e5b4ad
sushant2931 <sushantlad8888@gmail.com>
2021-11-12 17:57:11 +0530

    ADDED : Test Case To Calculate Fare For Less Distance Minimum Fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -3,9 +3,13 @@ package com.bridgelabz;
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM = 10;
     private static final int COST_PER_MIN = 1;
+    private static final int MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KM + time * COST_PER_MIN;
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_MIN;
+        if (totalFare <= MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 
 }
--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test;
 
 public class CabInvoiceTest {
     @Test
-    void givenDistanceAndTime_ShouldReturnTotalFare() {
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double totalFare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, totalFare);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double minFare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5.0, minFare);
     }
 }
 
